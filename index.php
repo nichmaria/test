a971a7474919cf4b21bb6dffaaffc972430891bf
Maria <masha.nichiporenko@gmail.com>
2022-04-02 17:40:36 +0300

    Init test project

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Mary</h1>
+    <p>Welcome to Open Server</p>
+
+    <?php
+        $a = 2;
+        $b = 7;
+        $c = $a * $b;
+        echo ($c);
+        echo ('PHP is here');
+    ?>
+</body>
+</html>
