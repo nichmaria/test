6e4496611ecd67938cf971151aea315fb45d3371
Maria <masha.nichiporenko@gmail.com>
2022-04-08 09:29:56 +0300

    added a css file

--- index.php
+++ index.php
@@ -5,10 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    H1 { 
+        font-size: 200%; 
+        font-family: Times New Roman; 
+        color: #000080; 
+    }
+    p{
+        color: #878003;
+    }
+  </style>
 </head>
 <body>
-    <h1>Hello Mary</h1>
-    <p>Welcome to Open Server</p>
+    <h1>А. Блок</h1>
+    <p>
+        Ночь, улица, фонарь, аптека,<br>
+        Бессмысленный и тусклый свет.<br>
+        Живи еще хоть четверть века —<br>
+        Всё будет так. Исхода нет.
+    </p>
+    <p>
+        Умрешь — начнешь опять сначала<br>
+        И повторится всё, как встарь:<br>
+        Ночь, ледяная рябь канала,<br>
+        Аптека, улица, фонарь.
+    </p>
 
     <?php
         $a = 2;
